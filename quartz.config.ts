d30a37ef92b0df2063c951ebf5f03514d1bb424e
diasyrmus <61349905+diasyrmus@users.noreply.github.com>
2025-01-02 11:16:47 +0000

    Quartz sync: Jan 2, 2025, 11:16 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "null",
     },
     locale: "en-GB",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "diasyrmus.github.io/diasyrmus",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
