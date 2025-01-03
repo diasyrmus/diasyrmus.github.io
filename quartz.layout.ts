2527ea8c66f214fb52a1a38060fb94a7a33f7f4b
diasyrmus <61349905+diasyrmus@users.noreply.github.com>
2025-01-03 13:48:11 +0000

    Quartz sync: Jan 3, 2025, 1:48 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -21,6 +21,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+    Component.MobileOnly(Component.Explorer()),
   ],
   left: [
     Component.PageTitle(),
