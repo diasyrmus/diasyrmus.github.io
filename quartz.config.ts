2c0b38fe8f03de85549ea794e1d70dfe30d5abb9
diasyrmus <61349905+diasyrmus@users.noreply.github.com>
2025-04-21 13:22:43 +0100

    Quartz sync: Apr 21, 2025, 1:22 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -76,7 +76,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
-      Plugin.Dataview(),
+      /** Plugin.Dataview(), **/
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
