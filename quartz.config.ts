44cb1c8e0204ea98a658adda96ab3300d8f328d6
diasyrmus <61349905+diasyrmus@users.noreply.github.com>
2025-04-21 13:30:21 +0100

    Quartz sync: Apr 21, 2025, 1:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -58,7 +58,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
+        priority: ["filesystem", "frontmatter"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
