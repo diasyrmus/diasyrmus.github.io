430a075a4467d0f1665e931298dc8fd07e41968a
diasyrmus <61349905+diasyrmus@users.noreply.github.com>
2025-04-21 13:19:55 +0100

    Quartz sync: Apr 21, 2025, 1:19 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -67,8 +67,10 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
-      ({ enableYouTubeEmbed: true }),
+      Plugin.ObsidianFlavoredMarkdown
+      ({ enableInHtmlEmbed: false,
+      enableYouTubeEmbed: true 
+      }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
